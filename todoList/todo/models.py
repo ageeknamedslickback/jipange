9a33d3052a7d5764b9ec56f6654713de3240933c
Dumbledore <mathenge362@gmail.com>
2021-11-29 15:01:54 +0300

    feat: project model design
    Signed-off-by: Dumbledore <mathenge362@gmail.com>

--- todoList/todo/models.py
+++ todoList/todo/models.py
@@ -1,3 +1,44 @@
+"""TodoList models."""
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class AbstractBaseModel(models.Model):
+    """
+    An abstract base class model.
+
+    It provides shared fields that are self-updating.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        """Define class Meta options."""
+
+        abstract = True
+
+
+class TodoList(AbstractBaseModel):
+    """
+    Defines a Todo List.
+
+    This is a named list that contains Todo items
+    """
+
+    list_name = models.CharField(max_length=50)
+    description = models.TextField()
+
+
+class TodoListLine(AbstractBaseModel):
+    """Defines the items/reminders in a Todo List."""
+
+    todo_list = models.ForeignKey("TodoList", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    scheduled_at = models.DateTimeField(null=True, blank=True)
+    sub_line = models.ForeignKey("self", on_delete=models.CASCADE)
